637a741fc4858ad8058f15cfffda9a30c5a2e70c
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-09 17:40:30 +0200

    feat: bonus condition to switch img && button content

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const button = document.querySelector(`button`)
 const img = document.querySelector(`img`)
 button.addEventListener(`click`, function(){
-  img.src = `./img/yellow_lamp.png`
+  if (img.src.includes(`white_lamp.png`) ){
+    img.src = './img/yellow_lamp.png';
+    button.innerText = `spegni`
+  }
+  else if (img.src.includes(`yellow_lamp.png`) ){
+    img.src = './img/white_lamp.png';
+    button.innerText = `accendi`
+  }
 } )
